75f4bf38528dd244129a05cbc6f3af6a8346e399
Nikola Putnik <nikola.putnik@student.uclouvain.be>
2020-04-01 08:47:52 +0200

    ajout d un graph lu sur csv sur le covid19

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -34,5 +34,24 @@ def create_app(test_config=None):
     
     from . import auth
     app.register_blueprint(auth.bp)
+    
+    @app.route('/graph0')
+    def graph():
+        fileCSV = open('data/total_cases.csv','r')
+        
+        pays0 = fileCSV.readline().replace('\n','') # on lit la 1ere ligne (liste des pays)
+        pays1 = pays0.split(',')
+        nombre_pays = len(pays1)-2 # nombre de pays
+        liste_pays = pays1[2:nombre_pays+2] # liste de tous les pays
+        
+        
+        last_day = fileCSV.readlines()[-1].replace('\n','') # on lit la derniere ligne
+        liste_last_day = last_day.split(',')
+        nombre_pays_infecte = len(liste_last_day)-2 # nombre de pays infectes le dernier jour
+        liste_cas_last_day = liste_last_day[2:nombre_pays_infecte+2] # liste du nombre de cas par pays le dernier jour
+        
+        graphique = 'bar'
+        
+        return render_template('graphs/graph.html', type = graphique, labels = str(liste_pays), data = str(liste_cas_last_day))
 
     return app
